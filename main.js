45680c93f5a3633f418893eaf64bb408deb026f9
DMRobb77 <riffninja@live.com>
2025-03-28 11:58:47 -0500

    Implement binary tree traversal to map out moves Use level order tree traversal to map out the correct route from initial position to ending position.

--- main.js
+++ main.js
@@ -1,91 +1,90 @@
-const knightFactory = (startPosition, currentBoard) => {
-    const position = startPosition;
+const knightFactory = (currentBoard) => {
     const board = currentBoard;
 
-    
-    const spaceFactory = (coords, move1 = null, move2 = null, move3 = null, move4 = null, 
-        move5 = null, move6 = null, move7 = null, move8 = null) => ({
-            coords,
-            move1,
-            move2,
-            move3,
-            move4,
-            move5,
-            move6,
-            move7,
-            move8
-    })
+    const spaceFactory = (initCoords, move1 = null, move2 = null, move3 = null, move4 = null, 
+        move5 = null, move6 = null, move7 = null, move8 = null) => {
 
-    // const generatePossibleMoves = () =>{
-    //     let root = spaceFactory([0, 0]);
-    //     for(let x = 0; x <= currentBoard.boardDimensions[0]; x += 1){
-    //         for(let y = 0; y <= currentBoard.boardDimensions[1]; y += 1){
-    //             const space = spaceFactory([x, y]);
-    //             space.move1 = [x + 1, y + 2];
-    //             space.move2 = [x + 2, y + 1];
-    //             space.move3 = [x + 2, y - 1];
-    //             space.move4 = [x + 1, y - 2];
-    //             space.move5 = [x - 1, y - 2];
-    //             space.move6 = [x - 2, y - 1];
-    //             space.move7 = [x - 2, y + 1];
-    //             space.move8 = [x - 1, y + 2];
+            let coords = initCoords;
 
-    //             Object.keys(space).forEach(property => {
-    //                 if (space[property][0] < 0 || space[property][0] > 7
-    //                     || space[property][1] < 0 || space[property][1] > 7){
-    //                         space[property] = null;
-    //                     }
-    //             });
+            if (initCoords[0] < 0 || initCoords[0] > 7
+                || initCoords[1] < 0 || initCoords[1] > 7){
+                    coords = null;
+                } 
 
-    //             console.log(space);
-
-    //             root = space;
-    //         }
-    //     }   
-    //     console.log(`Root is ${root.coords}`);
-    // }
-
-
-
-
-    const generatePossibleMoves = () =>{
-        let root = spaceFactory([0, 0]);
-        for(let x = 0; x <= currentBoard.boardDimensions[0]; x += 1){
-            for(let y = 0; y <= currentBoard.boardDimensions[1]; y += 1){
-                const space = spaceFactory([x, y]);
-                space.move1 = [x + 1, y + 2];
-                space.move2 = [x + 2, y + 1];
-                space.move3 = [x + 2, y - 1];
-                space.move4 = [x + 1, y - 2];
-                space.move5 = [x - 1, y - 2];
-                space.move6 = [x - 2, y - 1];
-                space.move7 = [x - 2, y + 1];
-                space.move8 = [x - 1, y + 2];
-
-                Object.keys(space).forEach(property => {
-                    if (space[property][0] < 0 || space[property][0] > 7
-                        || space[property][1] < 0 || space[property][1] > 7){
-                            space[property] = null;
-                        }
-                });
-
-                console.log(space);
-
-                root = space;
+            return {
+                coords,
+                move1,
+                move2,
+                move3,
+                move4,
+                move5,
+                move6,
+                move7,
+                move8
             }
-        }   
-        console.log(`Root is ${root.coords}`);
     }
 
+    const generatePossibleMoves = (currentPosition) =>{
+        const x = currentPosition[0];
+        const y = currentPosition[1];
+        const space = spaceFactory([x, y]);
+        space.move1 = spaceFactory([x + 1, y + 2]);
+        space.move2 = spaceFactory([x + 2, y + 1]);
+        space.move3 = spaceFactory([x + 2, y - 1]);
+        space.move4 = spaceFactory([x + 1, y - 2]);
+        space.move5 = spaceFactory([x - 1, y - 2]);
+        space.move6 = spaceFactory([x - 2, y - 1]);
+        space.move7 = spaceFactory([x - 2, y + 1]);
+        space.move8 = spaceFactory([x - 1, y + 2]);
+        return space;
+    }
 
+    const levelOrder = (initPosition, targetString) => {
+  
+        const initQueue = [{ node: initPosition, path: [initPosition.coords]}];
 
-    const move = (target) => {
-        generatePossibleMoves();
+        const traverse = (queue) => {
+          if (queue.length >= 1){
+            const { node, path } = queue.shift();
+
+            if (node.coords && node.coords.toString() === targetString){
+                return path;
+            }
+
+            for (let i = 1; i < Object.keys(node).length; i += 1){
+        
+                const validMove = node[`move${  i}`].coords;
+
+                if (validMove){
+                    queue.push({node: generatePossibleMoves(validMove), 
+                        path: path.concat([validMove])});
+                }
+            }
+
+            return traverse(queue);
+          }
+        }
+
+        return traverse(initQueue);
+  
+      }
+
+    const move = (positionData) => {
+        const startPosition = positionData[0];
+        const targetString = positionData[1].toString();
+
+        console.log(`Traveling from ${startPosition} to ${targetString}...`);
+
+        const resultArray = levelOrder(generatePossibleMoves(startPosition), targetString);
+
+        console.log(`You made it in ${resultArray.length} moves! Here's your path:`);
+        resultArray.forEach(element => {
+            console.log(element);
+        });
 
     }
 
     return {
-        position,
         move
     }
 }
@@ -94,17 +93,10 @@ const knightFactory = (startPosition, currentBoard) => {
 const boardFactory = (boardMaxDimensions) => {
     const boardDimensions = boardMaxDimensions;
 
-
-
     return {boardDimensions};
 }
 
-
-
 const newBoard = boardFactory([7, 7]);
+const newKnight = knightFactory([1, 3], newBoard);
 
-const newKnight = knightFactory([0, 0], newBoard);
-
-newKnight.move(1,1);
-
-console.log(newKnight.position);
+newKnight.move([[2,7],[3,3]]);
